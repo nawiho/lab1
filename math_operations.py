ea631c59bfebfc17bc8470c92ae714e96d524e2a
nawi <krylova04122006@gmail.com>
2025-11-01 10:42:06 +0300

    Завершение лабораторной работы №5: bisect и cherry-pick

--- math_operations.py
+++ math_operations.py
@@ -1,3 +1,13 @@
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def square_root(x):
+    if x < 0:
+        raise ValueError("Cannot calculate square root of negative number")
+    return x ** 0.5
 
 def multiply(a, b):
     return a * b
